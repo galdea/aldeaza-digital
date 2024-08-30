e3277415f29523c9ef832e414922c90377d3be71
Gabriel Aldea <109429160+galdea@users.noreply.github.com>
2024-08-30 02:05:42 +0000

    contact form style fixed. but form is not working yet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,45 +1,44 @@
-import React, { useState } from 'react';
-import { useTranslation } from "react-i18next";
+import React from "react";
+import { useTranslation} from "react-i18next"
 
 export default function Contact() {
-  const { t } = useTranslation("global");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const {t, i18n} = useTranslation("global");
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("email", email);
-    formData.append("message", message);
-    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
-
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
-
-    try {
-      const response = await fetch("https://api.web3forms.com/submit", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        },
-        body: json
-      });
-      const result = await response.json();
-      if (result.success) {
-        console.log(result);
-        // Handle success feedback
-      } else {
-        // Handle error feedback
-        console.error("Form submission failed:", result.error);
-      }
-    } catch (error) {
-      console.error("Network error:", error);
-    }
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
   }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  
+    if (!emailRegex.test(email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+  
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => {
+        alert("Message sent, I will be contacting you soon!");
+        window.location.reload();
+      })
+      .catch((error) => alert(error));
+  }
+  
+
 
   return (
     <section id="contact" className="relative">
@@ -70,7 +69,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="custom-green leading-relaxed">
+              <a className="custom-green leading-relaxed md:pr-16">
                 gabriel.aldea@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
@@ -79,90 +78,73 @@ export default function Contact() {
               <p className="leading-relaxed">+56 993441532</p>
             </div>
           </div>
-          </div>
-          </div>
-
-      <div className="container px-5 py-10 mx-auto flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-        <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        </div>
         <form
+          netlify
           name="contact"
           onSubmit={handleSubmit}
-          className="flex flex-col md:w-1/2 lg:w-1/3"
-        >
-          
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium custom-green title-font">
-            {t("contact.title")}
-          </h2>
-          <p className="leading-relaxed mb-5">{t("contact.description")}</p>
-
+          {t("contact.title")}
+     </h2>
+          <p className="leading-relaxed mb-5 ">
+            
+ {t("contact.description")}
+          </p>
           <div className="relative mb-4">
-          <input type="hidden" name="access_key" value="f12fb2d3-f16a-41bc-8778-f148e3349578"></input>
-
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-              {t("contact.name")}
+            {t("contact.name")}
+
             </label>
             <input
               type="text"
               id="name"
               name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              required
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
-
           <div className="relative mb-4">
             <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-              {t("contact.email")}
+              Email
             </label>
             <input
               type="email"
               id="email"
               name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              required
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-
           <div className="relative mb-4">
             <label
               htmlFor="message"
-              className="leading-7 text-sm text-gray-400"
-            >
-              {t("contact.message")}
+              className="leading-7 text-sm text-gray-400">
+                            {t("contact.message")}
+
             </label>
             <textarea
               id="message"
               name="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              required
+              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
-
           <button
-            type="submit"
-            style={{ backgroundColor: '#c3d563', color: 'white' }}
-            className="border-0 py-2 px-6 focus:outline-none rounded text-lg hover:bg-gray-700"
-          >
-            {t("contact.submit")}
-          </button>
+  type="submit"
+  style={{
+    backgroundColor: '#c3d563', // Base color
+    color: 'white'
+  }}
+  className="border-0 py-2 px-6 focus:outline-none rounded text-lg hover:bg-gray-700"
+>
+  {t("contact.submit")}
+</button>
+
+
+
         </form>
       </div>
-      <a 
-  href="https://aldeazadigital.com" 
-  className='flex justify-center italic'
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Built by  <span className="aldeaza font-semibold ml-1">Aldeaza</span>
-</a>
-      </div>
-
     </section>
   );
 }
